c8e4893d5d5647645ce0bbeed52bf0a36211faf7
Brenda <brendamvreal@gmail.com>
2022-05-26 12:06:49 -0300

    Refactor code to remove hard coded values to add real data.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -3,13 +3,16 @@ import matchData from "../data/matchData";
 import playerData from "../data/playerData";
 import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers"
 
-function PlayerList() {
+function PlayerList(props) {
+
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
 }
