60425ce100257f919adc7a2720123f7a0f614034
Brenda <brendamvreal@gmail.com>
2022-05-26 11:34:06 -0300

    Create mock data for app.

---
+++ src/data/matchData.js
@@ -0,0 +1,40 @@
+const matchData = [
+  {
+    matchNumber: 1,
+    players: ["Disguised Lizard", "DrSpiteful"],
+    winner: "DrSpiteful",
+    scoreDifference: 1
+  },
+  {
+    matchNumber: 2,
+    players: ["Friiko", "Rodzcar"],
+    winner: "Friiko",
+    scoreDifference: 2
+  },
+  {
+    matchNumber: 3,
+    players: ["HolyMoly", "Captain Depp"],
+    winner: "HolyMoly",
+    scoreDifference: 1
+  },
+  {
+    matchNumber: 4,
+    players: ["Captain Depp", "HolyMoly"],
+    winner: "HolyMoly",
+    scoreDifference: 3
+  },
+  {
+    matchNumber: 5,
+    players: ["Domizera", "P1k4"],
+    winner: "P1k4",
+    scoreDifference: 1
+  },
+  {
+    matchNumber:6,
+    players: ["Domizera", "P1k4"],
+    winner: undefined,
+    scoreDifference: undefined
+  }
+]
+
+export default matchData;
