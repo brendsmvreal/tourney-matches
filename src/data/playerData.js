7e53f3399bf91502e8f53b392e2b37311d3dec4b
Brenda <brendamvreal@gmail.com>
2022-05-26 11:34:19 -0300

    Create mock data for app.

---
+++ src/data/playerData.js
@@ -0,0 +1,46 @@
+const playerData = {
+
+  "Disguised Lizard": {
+    gamerTag: "Disguised Lizard",
+    firstName: "Daria ",
+    lastName: "Stark",
+  },
+  "DrSpiteful": {
+    gamerTag: "DrSpiteful",
+    firstName: "Terrell",
+    lastName: "Hunter",
+  },
+  "Friiko": {
+    gamerTag: "Friiko",
+    firstName: "Ian",
+    lastName: "Sode",
+  },
+  "Rodzcar": {
+    gamerTag: "Rodzcar",
+    firstName: "Rodi",
+    lastName: "Herdeiro",
+  },
+  "HolyMoly": {
+    gamerTag: "HolyMoly",
+    firstName:"Holly",
+    lastName: "Length", 
+  },
+  "Captain Depp": {
+    gamerTag: "Captain Depp",
+    firstName: "Jack",
+    lastName: "Sparrow",
+  },
+  "Domizera": {
+    gamerTag: "Domizera",
+    firstName: "Dominique",
+    lastName: "Hardi",
+  },
+  "P1k4": {
+    gamerTag: "P1k4",
+    firstName: "Ralph",
+    lastName: "Lauren"
+  }
+}
+
+export default playerData;
+
