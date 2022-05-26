c15d54a6773f1bc894851c264caec26b71d27890
Brenda <brendamvreal@gmail.com>
2022-05-26 13:43:07 -0300

    Refactor code to render conditional data.

--- src/components/Match.js
+++ src/components/Match.js
@@ -3,9 +3,8 @@ import React from "react"; //optional
 function Match(props) {
   return (
     <article className="Match">
-      <h1>Joe <span>vs</span> Bob</h1>
-      {/* To be shown when there is a winner */}<h2>Joe is the winner by 1!</h2>
-      {/* To be shown when there is no winner */}<h2>No winners yet!</h2>
+     {props.winner && <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>}
+     {!props.winner && <h2>No winners yet!</h2>}
     </article>
   )
 }
